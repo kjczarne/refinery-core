ae189a9f17f3c8bdc80a320600d7a2d890b72215
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-09-18 23:43:21 +0200

    refactor: pulled engines back into core

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Refinery Core
 
-This repo contains core shared elements used both by Engines repo and the API repo. This covers stuff like `configProvider`, `IRecord` interface and `Record` class as well as SQL utils and general utility functions (including logger instance).
+This repo contains core shared elements used both by the API repo. This covers stuff like Engines, `configProvider`, `IRecord` interface and `Record` class as well as SQL utils and general utility functions (including logger instance).
 
-#TODO: integrate this repo with Engines and discontinue committing here
+To declare your own Engines, you can inherit from the `BaseEngine` class and implement `importCallback()` method for ingress and `exportCallback()` for egress. Then, import your engines in `yourEngines.ts` in the base repository and pass the engine type to the exported list. Don't forget to run `tsc`!
+
+This repo also contains the PouchDb database wrapper that interfaces to the CouchDb instance. It's placed here because `baseEngine` needs to know how to `find` docs and when I placed it in `core`, the `find` plugin didn't work upon import.
--- index.js
+++ index.js
@@ -18,4 +18,17 @@ __exportStar(require("./built/src/utilities/utils"), exports);
 var configProvider_1 = require("./built/src/configProvider");
 Object.defineProperty(exports, "config", { enumerable: true, get: function () { return configProvider_1.config; } });
 Object.defineProperty(exports, "DEFAULT_CONFIG_PATH", { enumerable: true, get: function () { return configProvider_1.DEFAULT_CONFIG_PATH; } });
+Object.defineProperty(exports, "__esModule", { value: true });
+var baseEngine_1 = require("./built/engines/baseEngine");
+Object.defineProperty(exports, "BaseEngine", { enumerable: true, get: function () { return baseEngine_1.BaseEngine; } });
+var markdown_1 = require("./built/engines/markdown");
+Object.defineProperty(exports, "MdEngine", { enumerable: true, get: function () { return markdown_1.MdEngine; } });
+var iBooks_1 = require("./built/engines/iBooks");
+Object.defineProperty(exports, "AppleiBooksEngine", { enumerable: true, get: function () { return iBooks_1.AppleiBooksEngine; } });
+var json_1 = require("./built/engines/json");
+Object.defineProperty(exports, "JsonEngine", { enumerable: true, get: function () { return json_1.JsonEngine; } });
+var andevFlashcards_1 = require("./built/engines/andevFlashcards");
+Object.defineProperty(exports, "AndevFldsEngine", { enumerable: true, get: function () { return andevFlashcards_1.AndevFldsEngine; } });
+var database_1 = require("./built/database");
+Object.defineProperty(exports, "RefineryDatabaseWrapper", { enumerable: true, get: function () { return database_1.RefineryDatabaseWrapper; } });
 //# sourceMappingURL=index.js.map
